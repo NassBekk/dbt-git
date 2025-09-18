47688a0a806f03e18408808e4aa7215e16c4dacf
Nassim <nassimbekkat@protonmail.com>
2025-09-18 14:16:11 +0000

    tot

---
+++ models/test_macro/boucle_if.sql
@@ -0,0 +1,12 @@
+
+select
+{%- for country in countries %}
+{%- if ' ' not in country %}
+count(case when name = '{{country}}' then name end) as {{country}}_amount
+{%- else %}
+{% set country_underscore = country.replace(' ', '_') -%}
+count(case when name = '{{country}}' then name end) as {{country_underscore}}_amount
+{% endif -%}
+{%- if not loop.last %},{% endif -%}
+{% endfor -%}
+from {{ ref('country_codes') }}
